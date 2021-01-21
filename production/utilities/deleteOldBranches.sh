6c5ba4e29d2cbfa6ddab76537dd0dafcababd690
Bret Hess <bret.hess@gmail.com>
2021-01-20 17:08:34 -0700

    edit deleteOldBranches.sh

--- production/utilities/deleteOldBranches.sh
+++ production/utilities/deleteOldBranches.sh
@@ -8,6 +8,6 @@ for branch in $(git branch -a | sed 's/^\s*//' | sed 's/^remotes\///' | grep -v 
     $ECHO git branch -D $local_branch_name
 #    $ECHO git branch -d $local_branch_name
 #    $ECHO git push origin --delete $local_branch_name
-    $ECHO git fetch -p  #prunes branches as well
   fi
 done
+$ECHO git fetch -p  #prunes branches as well
