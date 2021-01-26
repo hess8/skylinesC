fd99edce18ec45fba9d940ae4a113db498cb65fc
Bret Hess <bret.hess@gmail.com>
2021-01-26 09:57:08 -0700

    ufw changes to update_skylines.py

--- production/utilities/backup_skylines.py
+++ production/utilities/backup_skylines.py
@@ -165,7 +165,7 @@ while run:
     os.system('sudo ufw enable') > /dev/null 2>&1
     os.system('sudo ufw deny 4200') > /dev/null 2>&1
     os.system('sudo ufw deny 80') > /dev/null 2>&1
-    os.system('sudo ufw allow from 192.168.1.39 to any port 4200')
+    os.system('sudo ufw allow from 192.168.1.39 to any port 4200') > /dev/null 2>&1
 
     #find time until midnight
     secMidnight = ((24 - now.hour - 1) * 3600) + ((60 - now.minute - 1) * 60) + (60 - now.second)
