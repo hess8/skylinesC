beab4c6ab4db2bb1bbcf5b3ff72c8e5e3a357223
Bret Hess <bret.hess@gmail.com>
2021-02-04 12:26:37 -0700

    copied installFirst, Last from installFromScratch branch

--- installLast.sh
+++ installLast.sh
@@ -24,7 +24,6 @@ sudo apt-get install build-essential
 # add PPAs
 
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-# sudo add-apt-repository -y ppa:jonathonf/python-2.7 #not compa
 
 # update apt-get repository
 
@@ -32,36 +31,27 @@ sudo apt-get update
 
 # install base dependencies
 
-sudo apt-get install -y --no-install-recommends \
-   python python-dev \
-
+sudo apt-get install -y --no-install-recommends python python-dev 
+    
 sudo apt-get install -y --no-install-recommends \
     g++-6 pkg-config libcurl4-openssl-dev redis-server\
-    libpq-dev libfreetype6-dev libpng-dev libffi-dev
-echo 'New libs:'
-sudo apt-get install -y --no-install-recommends \
-    ibgeos-c1 liblwgeom-2.2-5
+    libpq-dev libfreetype6-dev libpng-dev libffi-dev 
+# echo 'New libs:'
+# sudo apt-get install -y --no-install-recommends \
+#     ibgeos-c1 liblwgeom-2.2-5
 
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt trusty-pgdg main" >> /etc/apt/sources.list'
-wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
-sudo apt update
-sudo apt install -y postgresql-10
-sudo apt install -y postgresql-10-postgis-2.4
-sudo apt install -y postgresql-10-postgis-scripts
-sudo apt install -y postgis
-sudo apt install -y postgresql-10-pgrouting
+# sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt bionic-pgdg main" >> /etc/apt/sources.lis't
+# wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
+# sudo apt update
+# sudo apt install -y postgresql-10
+# sudo apt install -y postgresql-10-postgis-2.4
+# sudo apt install -y postgresql-10-postgis-scripts
+# sudo apt install -y postgis
+# sudo apt install -y postgresql-10-pgrouting
 
+# # set GCC 6 as default
 
-##Lines from vagrant file:
-#sudo apt-get install -y --no-install-recommends \
-#    python python-dev \
-#    g++-6 pkg-config libcurl4-openssl-dev git redis-server \
-#    libpq-dev postgresql-9.5-postgis-2.2 postgresql-9.5-postgis-2.2-scripts postgresql-contrib-9.5 \
-#    libfreetype6-dev libpng-dev libffi-dev
-
-# set GCC 6 as default
-
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 60 --slave /usr/bin/g++ g++ /usr/bin/g++-6
+# sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 60 --slave /usr/bin/g++ g++ /usr/bin/g++-6
 
 # sudo -H pip install pipenv
 # sudo apt-get install -y pkg-config
@@ -104,23 +94,23 @@ sudo pipenv install --verbose  immobilus
 sudo pipenv install --verbose  blinker
 
 
-# sudo pipenv install --verbose --verbose --dev  --skip-lock #this doesn't work with skip-lock...seems to work without it, but can't lock anyway due to oauth dependency problems
+sudo pipenv install --verbose --verbose --dev  --skip-lock #this doesn't work with skip-lock...seems to work without it, but can't lock anyway due to oauth dependency problems
 
-# create PostGIS databases
+create PostGIS databases
 
-# sudo sudo -u postgres createuser -s $USER
+sudo sudo -u postgres createuser -s $USER
 
-# sudo sudo -u postgres createdb skylines -O $USER
-# sudo sudo -u postgres psql -d skylines -c 'CREATE EXTENSION postgis;'
-# sudo sudo -u postgres psql -d skylines -c 'CREATE EXTENSION fuzzystrmatch;'
+sudo sudo -u postgres createdb skylines -O $USER
+sudo sudo -u postgres psql -d skylines -c 'CREATE EXTENSION postgis;'
+sudo sudo -u postgres psql -d skylines -c 'CREATE EXTENSION fuzzystrmatch;'
 
-# sudo sudo -u postgres createdb skylines_test -O $USER
-# sudo sudo -u postgres psql -d skylines_test -c 'CREATE EXTENSION postgis;'
-# sudo sudo -u postgres psql -d skylines_test -c 'CREATE EXTENSION fuzzystrmatch;'
+sudo sudo -u postgres createdb skylines_test -O $USER
+sudo sudo -u postgres psql -d skylines_test -c 'CREATE EXTENSION postgis;'
+sudo sudo -u postgres psql -d skylines_test -c 'CREATE EXTENSION fuzzystrmatch;'
 
-# sudo sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'secret123';"
+sudo sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'secret123';"
 
-# pipenv run ./manage.py db create
+pipenv run ./manage.py db create
 
 # create folder for downloaded files
 mkdir -p htdocs/files
@@ -128,48 +118,37 @@ mkdir -p htdocs/srtm
 
 # Front end
 #add-apt-repository may not be present on some Ubuntu releases:
-sudo apt-get install python-software-properties
+# sudo apt-get install python-software-properties
 
-sudo add-apt-repository -y -r ppa:chris-lea/node.js
-sudo rm -f /etc/apt/sources.list.d/chris-lea-node_js-*.list
-sudo rm -f /etc/apt/sources.list.d/chris-lea-node_js-*.list.save
-curl -sSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
-VERSION=node_12.x
-DISTRO="$(lsb_release -s -c)"
-echo "deb https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-sudo apt-get update
-sudo apt-get install -y nodejs
+# sudo add-apt-repository -y -r ppa:chris-lea/node.js
+# sudo rm -f /etc/apt/sources.list.d/chris-lea-node_js-*.list
+# sudo rm -f /etc/apt/sources.list.d/chris-lea-node_js-*.list.save
+# curl -sSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
+# VERSION=node_12.x
+# DISTRO="$(lsb_release -s -c)"
+# echo "deb https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+# sudo apt-get update
+# sudo apt-get install -y nodejs
 
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-echo "deb-src https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee -a /etc/apt/sources.list.d/nodesource.list
+# curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+# echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+# echo "deb-src https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee -a /etc/apt/sources.list.d/nodesource.list
 
-sudo apt-get update && sudo apt-get install -y yarn
-sudo npm install -y -g bower
-sudo npm install -y -g ember-cli
+# sudo apt-get update && sudo apt-get install -y yarn
+# sudo npm install -y -g bower
+# sudo npm install -y -g ember-cli
 
-cd ember
-yarn -d #npm packages including dev
-sudo bower install --allow-root
-cd ../
-sudo chown $USER -R ~/.config/*
+# cd ember
+# yarn -d #npm packgas including dev
+# sudo bower install --allow-root
+# cd ../
+# sudo chown $USER -R ~/.config/*
 
-# management
-#pipenv run ./manage.py import welt2000 --commit
+# # management
+# #pipenv run ./manage.py import welt2000 --commit
 
-## elevation data.  Make sure that you have 48 GB extra available for postgresql to grow!
-## don't want to do the download and extracting each time. Importing takes a long time
-#wget -i tiles.txt -P downloads -c --quiet
-#unzip -j -d unzipped "downloads/*.zip"
-##import to database.  Make sure disks are not saturated by other usage.  Will run while database is running
-#raster2pgsql -a -e -s 4326 -t 100x100 /media/sf_srtmSkylinesCondor/*.hgt elevations | psql -d skylines > elevationsImportLog.txt
-##if have this elevations dump, it might be faster, but I'm not sure it can run when the database is running:
-#pg_restore -d skylines  --data-only  -t elevations /media/sf_srtm/srtmElevDump.custom
-
-
-# production server
+# # production server
 # sudo ufw enable
 # systemctl status nginx
 
-#pgadmin
 
