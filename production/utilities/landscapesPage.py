9de8e7e9e0c1812a22af30d5a3ce0c9ad66312f6
Bret Hess <bret.hess@gmail.com>
2021-02-05 14:44:52 -0700

    Changed landscapes.py and en.yaml to host only magnet links

--- production/utilities/landscapesPage.py
+++ production/utilities/landscapesPage.py
@@ -23,44 +23,41 @@ lines.append('<BasePage> \n')
 lines.append('  <div class="page-header"> \n')
 lines.append('    <h1>{{t "landscapes"}}</h1> \n')
 lines.append('  </div> \n')
-lines.append('  <p> {{t "landscapes-download"}} </p> \n')
-# lines.append('  <p> {{t "install"}} <a href="https://www.fosshub.com/qBittorrent.html">  {{"qBittorrent"}}</a> {{t "qbittorent"}} <b> {{t "torrent"}} </b> {{t "torrent-after"}}</p> \n')
-lines.append('  <p> {{t "install"}} <a href="https://www.fosshub.com/qBittorrent.html">  {{"qBittorrent"}}</a> {{t "qbittorent"}} <b> {{t "torrent"}} </b> </p> \n')
+lines.append('  <p> {{t "landscapesPage.download"}} </p> \n')
+lines.append('  <p> {{t "install"}} <a href="https://www.fosshub.com/qBittorrent.html">  {{"qBittorrent"}}</a> {{t "landscapesPage.qbittorent"}} <b> {{t "landscapesPage.before"}} </b> {{t "landscapesPage.magnet"}} </p> \n')
 
-lines.append('  <p> {{t "extract-with"}}  <a href="https://www.7-zip.org/download.html"> 7-zip </a>  {{t "landscapes-paste"}} </p> \n')
-lines.append('  <hr> \n')
-lines.append('  <p> <b> {{t "torrent-share"}} </b> {{t "torrent-seed"}} <b> {{t "torrent-easy"}} </b> {{t "torrent-run"}}</p> \n')
+lines.append('  <p> {{t "landscapesPage.extract-with"}}  <a href="https://www.7-zip.org/download.html"> 7-zip </a>  {{t "landscapesPage.paste"}} </p> \n')
 
 lines.append('  <hr> \n')
-lines.append('  <p> {{t "torrent-magnet"}}  <b> {{t "torrent-maglink"}} </b> {{t "torrent-magopen"}}  </p> \n')
-lines.append('  <p> <hr> </p> \n')
+lines.append('  <p> <b> {{t "landscapesPage.share"}} </b> {{t "landscapesPage.seed"}} <b> {{t "landscapesPage.easy"}} </b> {{t "landscapesPage.run"}}</p> \n')
+
 
 lines.append('  <div class ="col-md-4" > \n')
-lines.append('  <p> {{fa-icon "envelope"}} <a href = "mailto:{{' + "'skylinescondor@gmail.com'}}" + '"> {{t "contact-admin"}} </a> {{" "}} {{t "contact-torrents"}} </p>   \n')
+lines.append('  <p> {{fa-icon "envelope"}} <a href = "mailto:{{' + "'skylinescondor@gmail.com'}}" + '"> {{t "contact-admin"}} </a> {{" "}} {{t "landscapesPage.contact"}} </p>   \n')
 lines.append('  </div> \n')
 lines.append('  <p> <hr> </p> \n')
 
 lines.append('<table class="table table-striped"> \n')
 lines.append('  <thead> \n')
-lines.append('        <th class="column-buttons"> {{t "download-torrent"}}</th> \n')
-lines.append('        <th class="column-buttons"></th> \n')
-lines.append('        <th class="column-buttons"></th> \n')
+# lines.append('        <th class="column-buttons"> {{t "download-torrent"}}</th> \n')
+# lines.append('        <th class="column-buttons"></th> \n')
+# lines.append('        <th class="column-buttons"></th> \n')
 lines.append('  </thead> \n')
 
 lines.append('  <tbody> \n')
 
+lines.append('  <p> {{t "landscapesPage.see"}}  <a href="https:www.condor.club/sceneriesmap/241/"> {{t "landscapesPage.map"}} </a>  {{t "landscapesPage.locations"}} </p> \n')
+
 for i, name in enumerate(names):
     lines.append('\t<tr> \n')
-    lines.append('\t\t<td> <a href="http://199.192.98.227:8080/landscapes-zip/{}.torrent" download>'.format(name) + ' {{fa-icon "download" size="sm"}}' + ' {} </a> </td> \n'.format(name.replace('.7z','')))
+    # lines.append('\t\t<td> <a href="http://199.192.98.227:8080/landscapes-zip/{}.torrent" download>'.format(name) + ' {{fa-icon "download" size="sm"}}' + ' {} </a> </td> \n'.format(name.replace('.7z','')))
     magfilepath = dir+'/{}.magnet'.format(name)
-    # print magfilepath
-    if os.path.exists(magfilepath):
-        print name
-        magline = readfileNoStrip(magfilepath)[0].strip() + trackerStr
-        print 'mag',magline
-        lines.append('\t<td> <a href="{}"> magnet </a> </td> '.format(magline))
+    print name
+    magline = readfileNoStrip(magfilepath)[0].strip() + trackerStr
+    lines.append('\t\t<td> <a href="{}">'.format(magline) + ' {{fa-icon "download" size="sm"}}' + ' {} </a> </td> \n'.format(name.replace('.7z','')))
+    # lines.append('\t<td> <a href="{}"> magnet </a> </td> '.format(magline))
     sizeStr = '{:.1f} GB"'.format(sizes[i] /float(10 ** 9))
-    lines.append('\t\t<td align = "right"> {{"' + sizeStr  + '}} </td> \n')
+    lines.append('\t<td align = "right"> {{"' + sizeStr  + '}} </td> \n')
     lines.append('\t</tr> \n\n')
 
 lines.append('  </tbody> \n')
