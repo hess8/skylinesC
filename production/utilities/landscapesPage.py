70b690283fca1f1261f745037860ce407a4abb55
Bret Hess <bret.hess@gmail.com>
2021-02-14 17:21:28 -0700

    fixed link in landscapesPage.py

--- production/utilities/landscapesPage.py
+++ production/utilities/landscapesPage.py
@@ -24,7 +24,7 @@ lines.append('  <div class="page-header"> \n')
 lines.append('    <h1>{{t "landscapes"}}</h1> \n')
 lines.append('  </div> \n')
 lines.append('  <p> {{t "landscapesPage.download"}} </p> \n')
-lines.append('  <p> {{t "install"}} <a href="https://www.fosshub.com/qBittorrent.html">  {{"qBittorrent"}}</a> {{t "landscapesPage.qbittorent"}} <b> {{t "landscapesPage.before"}} </b> {{t "landscapesPage.magnet"}} </p> \n')
+lines.append('  <p> {{t "install"}} <a href="https://www.fosshub.com/qBittorrent.html">  {{"qBittorrent"}}</a> {{t "landscapesPage.qbittorent"}} <b> {{t "landscapesPage.before"}} </b> {{t "landscapesPage.many"}} {{t "landscapesPage.magnet"}} </p> \n')
 
 lines.append('  <p> {{t "landscapesPage.extract-with"}}  <a href="https://www.7-zip.org/download.html"> 7-zip </a>  {{t "landscapesPage.paste"}} </p> \n')
 
@@ -46,7 +46,7 @@ lines.append('  </thead> \n')
 
 lines.append('  <tbody> \n')
 
-lines.append('  <p> {{t "landscapesPage.see"}}  <a href="https:www.condor.club/sceneriesmap/241/"> {{t "landscapesPage.map"}} </a>  {{t "landscapesPage.locations"}} </p> \n')
+lines.append('  <p> {{t "landscapesPage.see"}}  <a href="https://www.condor.club/sceneriesmap/241/"> {{t "landscapesPage.map"}} </a>  {{t "landscapesPage.locations"}} </p> \n')
 
 for i, name in enumerate(names):
     lines.append('\t<tr> \n')
