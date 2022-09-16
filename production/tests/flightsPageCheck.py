45c1dcc4d232f0aae1f0f0906064d2d9815cd8e4
Bret Hess <bret.hess@gmail.com>
2022-09-15 21:02:26 -0600

    finished flightsPageCheck.py

--- production/tests/flightsPageCheck.py
+++ production/tests/flightsPageCheck.py
@@ -1,22 +1,29 @@
 import smtplib
 import requests
 from time import sleep
-from funcs import emailMessage
+from datetime import datetime
+from funcs import emailMessage, turns
 
 senderEmail = 'skylinesC@soardata.org'
 receiverEmail = 'bret.hess@gmail.com'
 looptime = 60 #sec
 alerttime = 300
-subject = 'SkylinesCondor flights page down'
+print(datetime.now().strftime("%m/%d/%Y %H:%M:%S"), end='\r')
 while True:
     try:
-        response = requests.get('https://skylinescondor.com/api/flights/',timeout=2)
+        response = requests.get('https://skylinescondor.com/api/flights/all?page=1',timeout=2)
         jsonr = response.json()
+        if jsonr['count']>0:
+            turns(looptime,'')
+            continue
+        else:
+            subject = 'Zero skylinesC flights count'
+            emailMessage(subject, '', senderEmail, receiverEmail)
+            turns(alerttime, 'zero count')
     except:
-        print('Timeout')
+        subject = 'SkylinesC flights page timeout/error'
         emailMessage(subject, '', senderEmail, receiverEmail)
-        sleep(alerttime)
-    sleep(looptime)
+        turns(alerttime,'timeout/error')
 
 
 
--- production/tests/funcs.py
+++ production/tests/funcs.py
@@ -1,6 +1,8 @@
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import smtplib
+from datetime import timedelta,datetime
+from time import sleep
 
 def emailMessage(subject, html, senderEmail,receiverEmail):
     message = MIMEMultipart("alternative")
@@ -10,3 +12,18 @@ def emailMessage(subject, html, senderEmail,receiverEmail):
     message.attach(MIMEText(html,"html"))
     with smtplib.SMTP('localhost') as server:
         server.sendmail(senderEmail,receiverEmail,message.as_string())
+
+def turns(delta, str):
+    blank = '                                         '  ##
+    start = datetime.now()
+    while datetime.now() < start + timedelta(seconds=delta):
+        print(blank, end='\r')
+        print(datetime.now().strftime("%m/%d/%Y %H:%M:%S"), '-', str, end='\r')
+        sleep(0.5)
+        print(datetime.now().strftime("%m/%d/%Y %H:%M:%S"), '\\', str, end='\r')
+        sleep(0.5)
+        print(datetime.now().strftime("%m/%d/%Y %H:%M:%S"), '!', str, end='\r')
+        sleep(0.5)
+        print(datetime.now().strftime("%m/%d/%Y %H:%M:%S"), '/', str, end='\r')
+        sleep(0.5)
+
