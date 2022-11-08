a6870dffd160a0c2764b3e19cb26cc3b86de74d9
Bret Hess <bret.hess@gmail.com>
2022-11-08 14:14:57 -0700

    email.py has "all" or "admin" switch

--- skylines/commands/users/email.py
+++ skylines/commands/users/email.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, print_function
 
 from flask_script import Command, Option
 
-import os
+import os,sys
 import smtplib
 from email.mime.text import MIMEText
 from flask import current_app
@@ -17,40 +17,50 @@ class Email(Command):
 
     option_list = (
         Option("path", help="path to a text file with the content of the email"),
+        Option("to", help="can be 'admin' (goes to ) or 'all'")
     )
 
-    def run(self, path):
-        os.chdir('/home/bret/servers/repo-skylinesC/skylinesC')
+    def sendEmail(self, user, sender, recipient, title, text):
+        print('Sending email to {} (ID: {})...'.format(user.name, user.id))
+        try:
+            body = 'Hi {},\n'.format(user.name.split(' ')[0])
+            body += text
+            msg = MIMEText(body)
+            msg["Subject"] = title
+            msg["From"] = sender
+            s = smtplib.SMTP('localhost')
+            s.sendmail(sender, [recipient], msg.as_string())
+            s.quit()
+        except BaseException as e:
+            print("Sending email failed: {}", e)
+
+
+    def run(self, path, to):
+
+        if to not in ['admin','all']:
+            sys.exit('Stop: last option must be "admin" or "all"')
         sender = 'skylinescondor@soardata.org'
+        os.chdir('/home/bret/servers/repo-skylinesC/skylinesC')
         lines = readfileNoStrip(path)
         title = lines[0].strip()
         text = ''
         for line in lines[1:]:
             text += line
-        text += '\nBret'
+        text += '\nBret @ SkylinesCondor'
         users_query = (
             db.session.query(User).filter(User.email_address != None).order_by(User.id)
         )
 
         for user in users_query:
-            if 'Hess' not in user.name:
-                continue ##########test
-            print(
-                u"Sending email to {} (ID: {})...".format(user.name, user.id).encode(
-                    "utf-8"
-                )
-            )
+            recipient = user.email_address
+            if to == 'admin' and user.admin:
+                self.sendEmail(user,sender,recipient,title, text)
+                break
+            elif to == 'all':
+                self.sendEmail(user,sender,recipient,title,text)
 
-            try:
-                recipient = user.email_address
-                body = 'Hi {},\n'.format(user.name.split(' ')[0])
-                body += text
-                msg = MIMEText(body)
-                msg["Subject"] = title
-                msg["From"] = sender
-                s = smtplib.SMTP('localhost')
-                s.sendmail(sender, [recipient], msg.as_string())
-                s.quit()
 
-            except BaseException as e:
-                print("Sending email failed: {}", e)
+
+
+
+
