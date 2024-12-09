05f5c5d74da8ba830f0a5aa45b8ce75dd8d67850
Bret Hess <bret.hess@gmail.com>
2024-12-09 10:47:58 -0700

    added renewSSL.sh again

---
+++ production/utilities/renewSSL.sh
@@ -0,0 +1,8 @@
+#run on nginx machine
+unlink /etc/nginx/sites-enabled/skylinescondor.com
+ln -s /etc/nginx/sites-available/acme-challenge /etc/nginx/sites-enabled
+systemctl restart nginx
+certbot renew # remove --dry run part after testing
+unlink /etc/nginx/sites-enabled/acme-challenge
+ln -s /etc/nginx/sites-available/skylinescondor.com /etc/nginx/sites-enabled
+systemctl restart nginx  
