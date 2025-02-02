7a19e145df99ecd87ec9719885453c8cdfd5cc90
Bret Hess <bret.hess@gmail.com>
2025-02-02 10:43:20 -0700

    restored flights/info.js

--- ember/app/routes/flights/info.js
+++ ember/app/routes/flights/info.js
@@ -2,13 +2,13 @@ import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
 export default Route.extend({
-//  account: service(),
-//  ajax: service(),
+  account: service(),
+  ajax: service(),
 
-//  model() {
-//    let userId = this.get('account.user.id');
-//    if (userId) {
-//      return this.ajax.request('/api/settings');
-//    }
-//  },
+  model() {
+    let userId = this.get('account.user.id');
+    if (userId) {
+      return this.ajax.request('/api/settings');
+    }
+  },
 });
