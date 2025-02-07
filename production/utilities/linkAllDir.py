f28f25caa3a59f9edcb26c8551db763e1da3c70a
bret <bret.hess@gmail.com>
2025-02-07 15:39:29 -0700

    small changed

--- production/utilities/linkAllDir.py
+++ production/utilities/linkAllDir.py
@@ -1,5 +1,8 @@
 from common import linkAllDir
-realDir = '/mnt/E/landscapes/landscapesC2-main/'
-linksDir = '/mnt/E/landscapes/landscapesC3-main/'
+realDir = 's:\\A\\landscapesC2'
+linksDir = 'C:\\condor2\\Landscapes'
+#realDir = '/mnt/E/landscapes/landscapesC2-main/'
+#linksDir = '/mnt/E/landscapes/landscapesC3-main/'
 
+#S:\A\landscapesC2
 linkAllDir(realDir,linksDir)
